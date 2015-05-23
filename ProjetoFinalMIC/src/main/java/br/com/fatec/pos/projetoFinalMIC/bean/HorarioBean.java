d659b3f3e332d660cd82b02a1c7b59517844cc19
mcgacarol <mcgacarol@gmail.com>
2015-05-22 21:39:24 -0300

    Implementação

--- ProjetoFinalMIC/src/main/java/br/com/fatec/pos/projetoFinalMIC/bean/HorarioBean.java
+++ ProjetoFinalMIC/src/main/java/br/com/fatec/pos/projetoFinalMIC/bean/HorarioBean.java
@@ -1,5 +1,63 @@
 package br.com.fatec.pos.projetoFinalMIC.bean;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.com.fatec.pos.projetoFinalMIC.dao.HorarioDAO;
+import br.com.fatec.pos.projetoFinalMIC.domain.Horario;
+
+@ManagedBean
+@ViewScoped
 public class HorarioBean {
-//MONIQUE
+
+	private Horario horario;
+	private List<Horario> horarios;
+
+	public void salvar() {
+		HorarioDAO horarioDAO = new HorarioDAO();
+		if (horario.getId() == null) {
+			horarioDAO.salvar(horario);
+			Messages.addGlobalInfo("Horario salvo com sucesso");
+		} else {
+			horarioDAO.editar(horario);
+			Messages.addGlobalInfo("Horario alterado com sucesso");
+		}
+		iniciar();
+	}
+
+	public void excluir() {
+		HorarioDAO horarioDAO = new HorarioDAO();
+		horarioDAO.excluir(horario);
+		Messages.addGlobalInfo("Horario excluído com sucesso");
+		iniciar();
+	}
+
+	@PostConstruct
+	public void iniciar() {
+		horario = new Horario();
+		HorarioDAO horarioDAO = new HorarioDAO();
+		horarios = horarioDAO.listar();
+	}
+
+	public Horario getHorario() {
+		return horario;
+	}
+
+	public void setHorario(Horario horario) {
+		this.horario = horario;
+	}
+
+	public List<Horario> getHorarios() {
+		return horarios;
+	}
+
+	public void setHorarios(List<Horario> horarios) {
+		this.horarios = horarios;
+	}
+
 }
